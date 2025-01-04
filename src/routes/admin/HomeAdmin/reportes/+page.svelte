06513c827a10d25768dd6ac34ee4aac4ddd3da23
SofiaW <117039398+SofiShiny@users.noreply.github.com>
2025-01-04 19:29:09 -0400

    a

--- src/routes/admin/HomeAdmin/reportes/+page.svelte
+++ src/routes/admin/HomeAdmin/reportes/+page.svelte
@@ -17,7 +17,6 @@
 </style>
 
 <div class="container">
-    <h1>Mi Página con JasperServer</h1>
     <iframe src='http://localhost:8081/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2Freports&reportUnit=%2Freports%2FProveedoresYSusProductos&standAlone=true'></iframe>
 </div>
 
